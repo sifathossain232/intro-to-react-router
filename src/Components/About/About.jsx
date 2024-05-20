2b51659a2041a5ef29491cc416f01c8c5f892a48
SIFAT HOSSAIN <143956074+sifathossain232@users.noreply.github.com>
2024-05-20 12:19:59 +0600

    h1 tag

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,6 +4,7 @@ const About = () => {
     return (
         <div>
             <h3 className='text-2xl font-medium mt-72'>This is the about real page</h3>
+            <h1>hellow</h1>
         </div>
     );
 };
